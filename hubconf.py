c96b21866d3c176dc835260db3a5b31e921b0cd2
Birgi Tamersoy <birgi.tamersoy@siemens-healthineers.com>
2019-07-31 12:20:14 +0200

    update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,7 +1,16 @@
-dependencies = ['torch']
+dependencies = ['torch','tensorflow']
 
-def HUB_MODEL(*args, **kwargs):
+def PYTORCH_MODEL(*args, **kwargs):
     import torch
     model = torch.hub.load('pytorch/vision', 'mobilenet_v2', pretrained=True)
+
+    return model
+
+def TENSORFLOW_MODEL(*args, **kwargs):
+    import tensorflow as tf
+
+    model = tf.keras.applications.MobileNetV2(input_shape=(100,100,3),
+                                              include_top=False,
+                                              weights='imagenet')
 
     return model
