f806ed2d3d048d3ea376db18e2ff9944d7fe4a98
b1rg1 <birgitamersoy@gmail.com>
2019-10-14 13:32:30 +0200

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -1,18 +1,18 @@
-dependencies = ['torch','tensorflow']
 
 def PYTORCH_MODEL(*args, **kwargs):
+    dependencies = ['torch']
     import torch
     model = torch.hub.load('pytorch/vision', 'mobilenet_v2', pretrained=True)
 
     return model
 
 def TENSORFLOW_MODEL(*args, **kwargs):
+    dependencies = ['tensorflow']
     import tensorflow as tf
 
     model = tf.keras.applications.MobileNetV2(input_shape=(224,224,3),
                                               include_top=False,
                                               weights='imagenet')
-
     return model
 
 def MY_MODEL(*args, **kwargs):
